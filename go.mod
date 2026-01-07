ad9c5cbf82b8d4a817c38098d90abe89c218a832
nbhaohao <nbhaohao@users.noreply.github.com>
2026-01-07 14:42:02 +0800

    init: add mymath package with Add and Multiply functions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nbhaohao/pudge-go-module
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,11 @@
+package pudge_go_module
+
+// Add 加法函数（首字母大写才能被外部包调用）
+func Add(a, b int) int {
+	return a + b
+}
+
+// Multiply 乘法函数
+func Multiply(a, b int) int {
+	return a * b
+}
